6741d0eeb99d921a90b5715fc1c805d7d50aff8d
Ondrej Drapalik <ondrej.drapalik@gmail.com>
2022-05-22 13:11:22 +0200

    chore: modified SignIn component

--- src/features/ui/components/Header/parts/SignIn/index.tsx
+++ src/features/ui/components/Header/parts/SignIn/index.tsx
@@ -4,8 +4,8 @@ import { Routes } from '~/features/core/constants/routes'
 
 export const SignIn = () => (
   <Link href={Routes.LOGIN}>
-    <a>
+    <div>
       Already have an account? <b>sign in</b>
-    </a>
+    </div>
   </Link>
 )
